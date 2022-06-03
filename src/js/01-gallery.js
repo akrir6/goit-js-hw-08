bb1bd3a60f952af690371fc88e86f080b797ac67
Roman Radakhevych <akrir@ukr.net>
2022-06-03 21:35:38 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 // import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// require('simplelightbox');
+import { galleryItems } from './gallery-items';
+
+// Change code below this line
+
+document.querySelector('div.gallery').insertAdjacentHTML(
+  'afterbegin',
+  galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>`;
+    })
+    .join('')
+);
+
+new SimpleLightbox('div.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
