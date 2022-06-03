b911bed9a2ce5be13aab5f8bfd40553cb9eedfe3
Roman Radakhevych <akrir@ukr.net>
2022-06-03 22:04:02 +0300

    deploy

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,6 @@
-// Add imports above this line
-// import { galleryItems } from './gallery-items';
-// Change code below this line
-
-// console.log(galleryItems);
-
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
-// require('simplelightbox');
 import { galleryItems } from './gallery-items';
-
-// Change code below this line
 
 document.querySelector('div.gallery').insertAdjacentHTML(
   'afterbegin',
